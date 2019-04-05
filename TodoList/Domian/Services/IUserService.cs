4fbb280c69e9e3d2d119352f57238c0613c8b917
Harvin Bejarano <harvin.bejarano@yuxiglobal.com>
2019-04-05 14:49:32 -0500

    first commit

---
+++ TodoList/Domian/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Domian.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+    }
+}
